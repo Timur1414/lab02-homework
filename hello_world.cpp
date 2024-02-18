6f631c5cad2c7295bb71369ec718166192f23e91
Тимур <63254053+Timur1414@users.noreply.github.com>
2024-02-18 14:10:44 -0500

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
     std::cout << "Enter name:\n";
     std::string name;
-    std::cin >> name;  // enter name
+    std::cin >> name;  // enter user's name
     std::cout << "Hello, world, from " << name << "." << std::endl;
     return 0;
 }
